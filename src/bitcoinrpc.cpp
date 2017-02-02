93670915bdbfd27a771736d8caa9bca9a5ac4510
OBAViJEST <morthor.spede@gmail.com>
2017-02-02 18:56:23 -0500

    Update bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 33828 : 33882;
+    return GetBoolArg("-testnet", false) ? 33882 : 33828;
 }
 
 Object JSONRPCError(int code, const string& message)
